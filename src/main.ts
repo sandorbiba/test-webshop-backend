99393a58d96cb01e4f137547ecce4287112bbe53
Sandor Biba <sandorbiba@gmail.com>
2021-05-17 15:13:24 +0200

    heroku

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   const configService: ConfigService = app.get(ConfigService);
 
   const config = new DocumentBuilder()
